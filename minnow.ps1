2f15480e52e89327089fe86349c7b18f897d7da2
pocket-country <william.h.robb@gmail.com>
2023-05-30 16:27:02 -0500

    minnow with bugs, before branch to convert src/current mechanism to a class in an attempt to fix

--- minnow.ps1
+++ minnow.ps1
@@ -5,63 +5,95 @@
 # - create an input file for sparrow so I don't have to embed all of the above into my scanner
 #
 # Note  that the third item above will require creating a data format to pass along position info
-# for the scanner along with txt so can report errors  correctly.  Here we are trading the reduction of 
-# complexity in terms of not having the scanner deal with comment parsing for increased complexity 
+# for the scanner along with txt so can report errors  correctly.  Here we are trading the reduction of
+# complexity in terms of not having the scanner deal with comment parsing for increased complexity
 # of tracking position for error messages.
 #
 # Also, always wanted to build a DFA/state machine for fun.  Old Skool Cook.  #
-# Choose to build the state transition table "in code" vs 
+# Choose to build the state transition table "in code" vs
 # more general purpose  method (see a useful set of blog posts at //)
 # Can I bang this out in an afternoon?  Have a lot of the thinking/design done ...
 
 # We are in a scripting language have to declare functions before use!  Just like the olden days.
-# helpers that work on input buffer
-function IsAtEnd() {
-    return ($current -eq $textLength)
+
+# class - in the Pascal sense, a data record, to hold an 'extended character - our IR that holds position info as well as character
+class EC {
+    [char] $byte;
+    [int] $atline;
+    [int] $atoffset;
+    #constructor
+    EC([char] $c, [int] $l, [int] $o) {
+        $this.byte = $c;
+        $this.atline = $l;
+        $this.atoffset = $o;
+    }
 }
-function Peek($n){ # look ahead n characters
+# holds internal representation we are building, an array of extended character objects.
+$eChars = New-Object -TypeName "System.Collections.ArrayList";
+
+# class to encapsulate the input text file/string/buffer ... like an actual class with methods.
+# I wanted to write a scrappy little script without this stuff, but ...
+# Wasted a full day of my life trying to find a weird bug.  Return from write, value in 
+# global $Current reverts to 0 on first iteration works fine on subsequent iterations.
+# Got lost in PS's weired scripty scope rules, returns, lack of type declarations at 'script' level
+# So am hoping more modern encapsulation/data structure will fix the problem.  Still no idea what bug is
+# Am gonna pretend I'm writing Java/C++/C#
+
+# helpers that work on input buffer
+function IsAtEnd() { #boolean - this seems to work despite crazy semantics and implicit converesion of poweershell return statement
+    return ($current -eq ($src.length))
+}
+function Peek($n) { # look at current (or subsequent) charater in input buffeer
     $tc = $src[$current + $n];
+    if ($doggit) {add-content $mlog " Peeking at $current + $n : $tc"}
     return $tc;
 }
-function Write($n){
-    #get current character, make an extended chr object.  line and current position are globals
+# these three functions update current pointer (note return tranforms into an object! UGG that several hours of my life i'll never get back.)
+# i.e. "consume" characters.
+function Write($n){ #  eat current character(s), make an extended chr object.
+    Write-Host "In Write";
     for ($i = 0; $i -lt $n; $i++) {
         $tc = $src[$current + $i];
-        $eChr = []::new($tc, $line, $current);
-        $eChrs.add($eChr);
+        $to = $current - $linePos;
+        $eChar = [EC]::new($tc, $lineNum, $to);
+        $eChars.add($eChar);
     }
-    return ($current + $n);
+    #Write-Host $current;
+    #Write-Host $n;
+    $script:current = $current + $n;
+    Write-Host $script:current;
 }
-function WriteBlank(){
-    #for use in  whitespace processing, write a space
-    $eChr = []::new(" ", $line, $current);
-    $eChrs.add($eChr);
-    return ($current++);
+function WriteBlank(){ #for use in  whitespace processing, write a space
+    $to = $current - $linePos;
+    $eChar = [EC]::new(" ", $lineNum, $to);
+    $eChars.add($eChar);
+    $script:current = $current + 1;
 }
 function Skip($n) { # skip ahead n characters
-    return ($current + $n);
+    $script:current = $current + $n;
 }
 
-# logging 
+# logging
 function LogAction( $action) {
     if ($doggit) {add-content $mlog " Action Called: $action"}
 }
 function LogState() {
-    if ($doggit) {add-content $mlog "In state ($state) looking at ($c)[($current + $n)]"}
+    if ($doggit) {add-content $mlog "In state $state looking at [$current]:$c"}
 }
 # comment "stack" is just a number for now.  Later, maybe store actual text in an actual stack.
-function PushComment($cs) {
-    return ($cs++);
+function PushComment() {
+    $script:commentStack++;
 }
-function PopComment($cs) {
-    return ($cs--);
+function PopComment() {
+    $script:commentStack--;
 }
 # global variables count comments and lines in input
-function BumpLine($lc) {
-    return (lc++);
-}
-function BumpComment($cc) {
-    return (cc++);
+function NewLine() { # process a new line.
+    # call after writing blank for or skipping newline.  This implicit coupling makes me uneasy.
+    $script:lineNum++;    # bump line counter;
+    $script:linePos = $current; # capture starting position of now current line;
+                                # which starts following the newline we just wrote/skipped
+    #this is where we would updatee counts of blank lines, lines containing a comment, etc.
 }
 
 #set up log for testing state dynamics
@@ -71,91 +103,95 @@ if ($doggit) {
     $null = new-item $mlog -force ;
 }
 
-# int main(int argc, char *argv[]) { }
+# global tracking  variables.
+$current = 0;               #current character position in input text
+$lineNum = 1;               #current line in input text
+$linePos = 0;               #position in text of first character of current line
+
+$commentStack = 0;          #actually a counter, how deep are we in nested comments.
+
+# statistics - counts of comments, blank lines, code lines, etc
+
+#initialize state loop
+$state = "beg";     # everyone just numbers these, but then ... which is which?
+                    # I'm gonna use a 3 chr mnemonic
+$iter = 0; # for setting up a 'safety' during debugging
+
+# read in SQL text and start processing here ... int main(int argc, char *argv[]) { }
 if ($args) {$script_name = $args} else {$script_name = "./testMinnow.sql"}
 $src = Get-Content $script_name -raw;
 
-# global tracking  variables.
-$current = 0;               #current character position in input text
-$textLength = $src.Length;  #reference for EOF detection
-$lines = 0;                 #text lines in input (count newlines)
-$comments = 0;              #comments found (nested count as one)
-
-# the state machine loop:
-# everyone just numbers these, but then ... which is which? 
-# I'm gonna use a 3 chr mnemonic
-$state = "beg";
-
-# for setting up a 'safety' during debugging
-$iter = 0;
-
 # first pass write everything out for clarity.  Then refactor into functions for efficiency.
 
+# the state machine loop:
 while (-not (IsAtEnd)) {
+    $iter++;
+    if ($iter -ge 4) {Write-Host "Hit The Wall!"; Break;} else {Write-Host "Iteration ($iter)"}
 
-    $iter++; 
-    if ($iter -ge 100) {Write-Host "Hit The Wall!"; Exit}
-    
     Switch ($state) {
         'beg' { #1 'home' or beginning
-            $c = Peek(1);
+            $c = Peek(0);
             LogState;
             switch ($c) {
                 '-'  {$state = 'ds1'; break;}
                 # not wired up at the moment '/'  {$state = 'bc1'; break;}
-                ' '  {$state = 'ws1'; 
-                        LogAction("write a blank"); 
-                        WriteBlank;
-                        break;}
-                '\t' {$state = 'ws1'; 
+                ' '  {$state = 'ws1';
                         LogAction("write a blank");
-                        WriteBlank;
+                        $current = WriteBlank;
                         break;}
-                '\n' {$state = 'ws1'; 
-                        LogAction("bump line count + write a blank"); 
-                        BumpLine;
-                        WriteBlank;
+                '\t' {$state = 'ws1';
+                        LogAction("write a blank");
+                        $current = WriteBlank;
                         break;}
-                default { $state = 'beg'; 
-                        LogAction("process a regular charcter"); 
-                        Write(1);
+                '\n' {$state = 'ws1';
+                        LogAction("write a blank for newline & process newline");
+                        # order is important.  These two calls are coupled functionally
+                        $current = WriteBlank;
+                        NewLine;
+                        break;}
+                default { $state = 'beg';
+                        LogAction("process a regular charcter");
+                        Write-Host "before write"
+                        Write-Host $current;
+                        $current = Write(1);
+                        Write-Host "After write"
+                        Write-Host $current;
                         break;}
             }
             break;
         }
-        'ds1' { #2 saw first dash of a double dash, look for second 
+        'ds1' { #2 saw first dash of a double dash, look for second
             $c = Peek(2);
             LogState;
             switch ($c) {
-                '-'  {$state = 'ds2'; 
-                        Skip(2);        # saw two dashes, jump  over 'em both
-                        $comments = PushComment; # and we are in a comment, boys
+                '-'  {$state = 'ds2';
+                        $current = Skip(2);  # second of two dashes, jump  over 'em both
+                        PushComment;         # and we are in a comment, boys
                         break;}
-                default {$state = 'beg'; 
-                        Write(2); #false alarm.  Write this character, and the prior dash which as it turns out did not signify  a comment.
+                default {$state = 'beg';
+                        $current = Write(2); #false alarm.  Write this character, and the prior dash which as it turns out did not signify  a comment.
                         break;}
             }
             break;
         }
-        'ds2' { #3 saw second dash of a double dash comment - so in a comment.  Chew it up till newline.
+        'ds2' { #3 here if saw second dash of a double dash comment - so in a comment.  Chew it up till newline.
             $c = Peek(1);
             LogState;
             switch ($c) {
-                '\n'  {$state = 'beg'; 
-                    LogAction("end double dash comment, inc. line and comment count"); 
-                    Skip(1);
+                '\n'  {$state = 'beg';
+                    LogAction("end double dash comment, inc. line and comment count");
+                    $current = Skip(1);
+                    NewLine;
                     PopComment;
-                    BumpComment;
-                    BumpLine;
                     break;}
-                default {$state = 'ds2'; 
-                    LogAction("process character inside double dash comment"); 
-                    Skip(1);
+                default {$state = 'ds2';
+                    LogAction("skip over character inside double dash comment");
+                    $current = Skip(1);
                     break;}
             }
             break;
         }
-        'bc1' { #4  
+        'bc1' { #4
             $c = Peek(1);
             LogState;
             switch ($c){
@@ -183,19 +219,24 @@ while (-not (IsAtEnd)) {
             $c = Peek(1);
             LogState;
             switch ($c) {
-                ' '  {$state = 'ws1'; Skip(1); break;}
-                '\t' {$state = 'ws1'; Skip(1); break;}
-                '\n' {$state = 'ws1'; Skip(1); BumpLine; break;}
-                default {$state = 'beg'; 
-                    LogAction("leaving white space, process regular character"); 
-                    Write(1);
+                ' '  {$state = 'ws1'; $current = Skip(1); break;}
+                '\t' {$state = 'ws1'; $current = Skip(1); break;}
+                '\n' {$state = 'ws1'; $current = Skip(1); NewLine; break;}
+                default {$state = 'beg';
+                    LogAction("leaving white space, process regular character");
+                    $current = Write(1);
                     break}
             }
             break;
         }
     } # end state switch
 } # end main while loop
+
 #if state = final then OK else error
+
 #any post processing
+Write-Host "post processng"
+$eChars;
+ConvertTo-Json -EnumsAsStrings -depth 10 -InputObject ($eChars) | Out-File "./test.json"
 
 # ###
