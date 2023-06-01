bbd3b2964c616853b6fd4f360e12d1ae4a44c33d
pocket-country <william.h.robb@gmail.com>
2023-06-01 02:11:10 -0500

    block comments now in

--- minnow.ps1
+++ minnow.ps1
@@ -63,7 +63,7 @@ class InputBuffer {
     # could put in handling for skipping/retreiving past end of output buffer.  Do i need too?
     [void] Skip($n) { #advance pointer without returning character 
         if ($this.src[$this.current] -eq "`n") { $this.lineNum++; $this.LineStart = $this.current; }
-        $this.current++
+        $this.current = $this.current + $n;
     }
     [string] GetChr() { # return current character, advance pointer
         $tc = $this.src[$this.current]; #maybe could do all this in return stmt but they are twichy in PS
@@ -77,6 +77,7 @@ class InputBuffer {
 }
 function Write($n){ # eat current character(s), make an extended chr object.
     for ($i = 0; $i -lt $n; $i++) {
+        ### TODO this is wrong if the prior character was a newline! b/c things have been incremented
         $extChr = [EC]::new($sqlIn.getChr(), $sqlIn.lineNum, ($sqlIn.current - $sqlIn.LineStart));
         [void] $eChars.add($extChr);
         # how to decouple and not have globals?
@@ -95,6 +96,16 @@ function LogState() {
     if ($doggit) {add-content $mlog "In state $state with:$c"}
 }
 # comment "stack" is just a number for now.  Later, maybe store actual text in an actual stack.
+# so we will make a class, but it is a silly one.
+Class CS {
+    [int] $CommentStack;
+
+    CS() {$this.CommentStack = 0}
+
+    [void] Push() {$this.CommentStack++}
+    [void] Pop()  {$this.CommentStack--}
+    [bool] InAComment() { return $this.CommentStack -Gt 0}
+}
 function PushComment() {
     $script:commentStack++;
 }
@@ -108,7 +119,7 @@ if ($doggit) {
     $null = new-item $mlog -force ;
 }
 # global tracking  variables.
-$commentStack = 0;          #actually a counter, how deep are we in nested comments.
+$commentStack = [CS]::new();  #Initialize the comment stack. How deep are we in nested comments?
 
 # Read SQL into a new input buffere object
 # int main(int argc, char *argv[]) { }
@@ -139,7 +150,7 @@ while (-not $sqlIn.IsAtEnd()) {
             LogState;
             switch ($c) {
                 '-'  {$state = 'ds1'; break;}
-                # not wired up at the moment '/'  {$state = 'bc1'; break;}
+                '/'  {$state = 'bc1'; break;}
                 ' '  {$state = 'ws1'; LogAction("write a blank"); WriteBlank; break;}
                 "`t" {$state = 'ws1'; LogAction("write a blank"); WriteBlank; break;}
                 "`n" {$state = 'ws1'; LogAction("write a blank for newline"); WriteBlank; break;}
@@ -155,8 +166,8 @@ while (-not $sqlIn.IsAtEnd()) {
             LogState;
             switch ($c) {
                 '-'  {$state = 'ds2';
-                        $sqlIn.Skip(2);  # second of two dashes, jump  over 'em both
-                        PushComment;         # and we are in a comment, boys
+                        $sqlIn.Skip(2);         # second of two dashes, jump  over 'em both
+                        $commentStack.Push();   # and we are in a comment, boys
                         break;}
                 default {$state = 'beg';
                         Write(2); #false alarm.  Write this character, and the prior dash which as it turns out did not signify  a comment.
@@ -168,10 +179,10 @@ while (-not $sqlIn.IsAtEnd()) {
             $c = $sqlIn.Peek(0);
             LogState;
             switch ($c) {
-                "`n"  {$state = 'beg';
+                "`n" {$state = 'beg';
                     LogAction("end double dash comment");
                     $sqlIn.Skip(1);
-                    PopComment;
+                    $commentStack.Pop();
                     break;}
                 default {$state = 'ds2';
                     LogAction("skip over character inside double dash comment");
@@ -180,29 +191,63 @@ while (-not $sqlIn.IsAtEnd()) {
             }
             break;
         }
-        'bc1' { #4
-            $c = $sqlIn.Peek(0);
+        'bc1' { #4 peeked at a slash might begin a block comment
+            $c = $sqlIn.Peek(1);
             LogState;
-            switch ($c){
-                '*'  {$state = 'bcc'; LogAction("start block commeent"); break}
-                default {$state = 'beg'; LogAction("process a regular charcter") break;}
+            switch ($c) {
+                '*'  {$state = 'bc2';  # Yes, block comment jump over slash star, push comment stack
+                        LogAction("Enter block comment");
+                        $sqlIn.skip(2);
+                        $commentStack.Push();
+                        break;}  
+                default {$state = 'beg';  # Nope, that was just a slash
+                        LogAction("process a regular charcter (false alarm)");
+                        Write(2); # So write this character, and the prior *.
+                    break;}
             }
             break;
         }
-        'bcc' { #5
+        'bc2' { #5 yep, saw a slash star followng def are in a new block comment. Top level or nested, chew through it.  
             $c = $sqlIn.Peek(0);
             LogState;
+            switch ($c) {
+                "*" {$state = 'bcq'; # might be end of block comment, delay write/skip and look at next byte; 
+                        LogAction("might be end");
+                        break;}
+                default {$state = 'bc2';
+                        LogAction("skip over character inside block comment"); 
+                        $sqlIn.Skip(1) #recall skip handles newline;
+                        break;}
+            }
             break;
         }
-        'bcq' { #6
-            $c = $sqlIn.Peek(0);
+        'bcq' { #6 saw a star inside a block comment.  Is this the end?
+            $c = $sqlIn.Peek(1);
             LogState;
+            switch ($c) {
+                "/" {$state = 'bce'
+                        LogAction("End block comment"); # Yes!  But still have a decision to make 
+                        $commentStack.Pop();
+                        break;}
+                default {$state = 'beg';
+                    LogAction("Skip over character inside block comment");
+                    sqlIn.skip(2);   #skip over the star (false alarm) and slash that form delimiter;
+                    break;
+                }
+            }
             break;
         }
-        'bce' { #7
-            $c = $sqlIn.Peek(0);
-            LogState;
-            break;
+        'bce' { #7 just ended a block comment.  But was it nested?  Don't look at the input buffer!
+                # Note if we didn't have to check the comment stack here
+                # we might as well usee tail.ps1/regex based minifier.
+                # This here is the crux of handling nested comments.
+            if ($commentStack.InAComment()) { # I wrote this whole damm program just to get to this if!
+                $sqlIn.skip(2);  # skip over star slash that ended comment, still in a nested comment.
+                $state = 'bc2'
+            } else {
+                $sqlIn.skip(2) # skip over star slash that ended comment, not in a nested comment
+                $state = 'beg';
+            }
         }
         'ws1' { # 8: keep skipping consecutive white space
             $c = $sqlIn.Peek(0);
@@ -228,20 +273,20 @@ while (-not $sqlIn.IsAtEnd()) {
 
 #any post processing
 Write-Host "Generating Output"
-
 # TODO need to set up flags or something to control input and output file names and set these output booleans
 
-$console = $false;
+$console = $true;
 if ($console) { #dump value to default output
     $eChars;
 }
-$json = $true;
+
+$json = $false;
 if ($json) { # write  in json format.  Warning, this the file probably by a factor of 10
     $outfile = "./minnow.json"
     ConvertTo-Json -EnumsAsStrings -depth 3 -InputObject ($eChars) | Out-File "./minnow.json"
 }
 
-$minify = $true;
+$minify = $false;
 if ($minify) { #output as minified text.  This looses origional position info but preserves SQL (with \s* -> ' ' and comments removed)
     $outfile = "./minnow.min";
     $stringBuilder = [System.Text.StringBuilder]::new()
